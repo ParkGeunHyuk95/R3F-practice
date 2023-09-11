235a733970d91821750a519f39c31ceba1d6d4f1
ParkGeunHyuk95 <rmsgur87@genmatefield.com>
2023-09-11 21:28:33 +0900

    FEAT: apply orbit controls, center using drei

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Canvas } from "@react-three/fiber";
-
+import { OrbitControls, Center } from "@react-three/drei";
 export const App = () => (
-  <Canvas>
-    <Shirt />
+  <Canvas camera={{ position: [-1, 0, 5.5], fov: 50 }}>
+    <Center>
+      <Shirt />
+    </Center>
+    <OrbitControls />
   </Canvas>
 );
 
