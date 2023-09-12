6286830cd611cc30337d5699027ee1fa79cd923a
ParkGeunHyuk95 <rmsgur87@genmatefield.com>
2023-09-12 20:32:39 +0900

    FEAT: apply shadow by using drei [AccmulativeShadows, RandomizedLight]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Center, useGLTF, Environment } from "@react-three/drei";
+import {
+  OrbitControls,
+  Center,
+  useGLTF,
+  Environment,
+  Backdrop,
+} from "@react-three/drei";
 import { useRef } from "react";
 import Shirt from "./component/Shirt";
-export const App = () => (
+import BackDrop from "./component/BackDrop";
+export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
   <Canvas
-    camera={{ position: [-1, 0, 5.5], fov: 50 }}
+    shadows
+    camera={{ position, fov }}
     eventSource={document.getElementById("root")}
     eventPrefix="client"
   >
     <ambientLight intensity={0.5} />
     <Environment preset="city" />
     <Center>
-      <Shirt />
+      <Shirt />1
+      <BackDrop />
     </Center>
     <OrbitControls />
   </Canvas>
---
+++ src/component/BackDrop.js
@@ -0,0 +1,25 @@
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+import React from "react";
+
+const BackDrop = () => {
+  return (
+    <AccumulativeShadows
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+    </AccumulativeShadows>
+  );
+};
+
+export default BackDrop;
