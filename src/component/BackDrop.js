329cfc05bebbc29715ea1bf1d9bc6e823a169d67
ParkGeunHyuk95 <rmsgur87@genmatefield.com>
2023-09-13 21:21:40 +0900

    FEAT: apply recoil state color at shadow using useframe

--- src/component/BackDrop.js
+++ src/component/BackDrop.js
@@ -1,9 +1,19 @@
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import React, { useRef } from "react";
+import { useRecoilValue } from "recoil";
+import { colorState } from "../atoms";
 
 const BackDrop = () => {
+  const shadows = useRef();
+  const color = useRecoilValue(colorState);
+  useFrame((state, delta) => {
+    easing.dampC(shadows.current.getMesh().material.color, color, 0.25, delta);
+  });
   return (
     <AccumulativeShadows
+      ref={shadows}
       temporal //그림자를 좀 더 부드럽게
       frames={60} // 그림자 누적에 사용되는 프레임 수, 높을수록 부드러운 그림자
       alphaTest={0.85} //그림자 불투명도
